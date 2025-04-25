7422ae1219eb726dc8eacebaafb2f305a283adf3
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-04-25 13:59:17 +0200

    test assets

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
 "use strict" ; 
+
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset;
+  
+    document.querySelector('.layer-back').style.transform = `translateY(${scrollTop * 0.8}px)`;
+    document.querySelector('.layer-middle').style.transform = `translateY(${scrollTop * 0.5}px)`;
+    document.querySelector('.layer-front').style.transform = `translateY(${scrollTop * 0.2}px)`;
+  });
+
+const title = document.querySelector('.title--big');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const limit = window.innerHeight * 0.5; 
+  
+    const progress = Math.min(scrollY / limit, 1); 
+  
+    title.style.transform = `translateY(-50%, -${progress * 100}%)`;
+    title.style.opacity = `${1 - progress}`;
+  });

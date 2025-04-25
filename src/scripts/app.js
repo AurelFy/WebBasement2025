009a50a7392d9a5fbf8a6e07486a22e2e2195f79
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-04-25 18:02:01 +0200

    final

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,17 +3,24 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-    const scrollTop = window.scrollY;
-  
-    document.querySelector('.layer-back').style.transform = `translateY(${scrollTop * 0.9}px)`;
-    document.querySelector('.layer-middle').style.transform = `translateY(${scrollTop * 0.5}px)`;
-    document.querySelector('.layer-front').style.transform = `translateY(${scrollTop * 0.4}px)`;
+"use strict" ; 
 
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset;
+  
+    document.querySelector('.layer-back').style.transform = `translateY(${scrollTop * 0.8}px)`;
+    document.querySelector('.layer-middle').style.transform = `translateY(${scrollTop * 0.5}px)`;
+    document.querySelector('.layer-front').style.transform = `translateY(${scrollTop * 0.2}px)`;
+  });
+
+const title = document.querySelector('.title--big');
+
+window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
     const limit = window.innerHeight * 0.7;
   
     const progress = Math.min(scrollY / limit, 1);
-    console.log(scrollY);
+  
     // -50% pour le centrage horizontal, -X% pour la montée verticale
     title.style.transform = `translate(-50%, -${progress * 100}%)`;
     title.style.opacity = `${1 - progress}`;
@@ -28,7 +35,7 @@ gsap.from(".text1", {
         start: "center bottom",
         end: "top top",
         scrub: true,
-        markers: true,
+        // markers: true,
     },
     y: -100,
     opacity: 0,
